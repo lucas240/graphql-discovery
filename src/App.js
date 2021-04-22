fa578dede39e73968512a213393c81494028e87a
lucas <lucasleproust@gmail.com>
2021-04-22 11:23:20 +0200

    add spacex graphql api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const LAUNCH_QUERY = gql`
+  {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const { loading, error, data } = useQuery(LAUNCH_QUERY);
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.launches.map(({ launch_date_utc, details, rocket, links }) => (
+    <div key={launch_date_utc}>
+      <h3>{rocket.rocket_name}</h3>
+      <p>date: {launch_date_utc}</p>
+      <p>details: {details}</p>
+      <a href={links.video_link}> launch link</a>
+    </div>
+  ));
 }
 
 export default App;
